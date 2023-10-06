abf97e6201e25d0046b5183cc1f290802c88927e
AzrodLeSang <122784144+AzrodLeSang@users.noreply.github.com>
2023-10-06 17:41:35 +0200

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// var content = document.getElementsByTagName("body")[0];
+
+//var darkMode = document.getElementById("darkmode");
+
+//darkMode.addEventListener("click", function () {
+//  darkMode.classList.toggle("active");
+// content.classList.toggle("dark");
+//});
